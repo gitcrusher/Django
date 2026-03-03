166712d458a4adb14acf50d76942c40c43803370
gitcrusher <aayushguptasoni@gmail.com>
2026-03-04 00:03:39 +0530

    commit

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from first_project.views import hello_geeks
+
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('geek/', hello_geeks),
 ]
