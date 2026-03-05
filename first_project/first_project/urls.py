d1112d9e2a5a29f53ef1471cf10dbaf59d95d747
gitcrusher <aayushguptasoni@gmail.com>
2026-03-05 06:03:30 +0530

    fix: specify integer type for dynamic_url path parameter

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -24,7 +24,7 @@ from first_project.views import hello_geeks
 
 urlpatterns = [
     path('', home, name = "home"),
-    path('<id>/<name>/', dynamic_url, name ="dynamic_url"),
+    path('<int:id>/<name>/', dynamic_url, name ="dynamic_url"),
     path('about-Us/', about, name = "about"),
     path('contact/', contact, name = "contact"),
     path('geek/', hello_geeks),
